44573836c2e26420f8baee7f9f1ea892d7e73982
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-05-20 14:26:23 +0900

    fix: 잘못된 파이어베이스 환경변수 주입 수정

--- horror-alram-front/src/config.js
+++ horror-alram-front/src/config.js
@@ -3,9 +3,9 @@ export const FIREBASE_CONFIG = {
   authDomain: "horror-alarm-c169a.firebaseapp.com",
   projectId: "horror-alarm-c169a",
   storageBucket: "horror-alarm-c169a.appspot.com",
-  messagingSenderId: process.env.REACT_APP_MEASUREMENT_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 export const SUPABASE_CONFIG = {
