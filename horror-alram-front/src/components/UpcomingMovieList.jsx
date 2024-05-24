f8dc9ded549381ca931fba2fb60968d73c44090c
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-05-24 10:06:48 +0900

    feat: 모바일 환경에서는 세로 스크롤로 보기로 바꿈

--- horror-alram-front/src/components/UpcomingMovieList.jsx
+++ horror-alram-front/src/components/UpcomingMovieList.jsx
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Container from '@mui/material/Container';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import ListSubheader from '@mui/material/ListSubheader';
+import { useMediaQuery } from '@mui/material';
+import { ImageList, ImageListItem, ImageListItemBar, ListSubheader } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import DialogContent from '@mui/material/DialogContent';
 
 export function UpcomingMovieList() {
-  const matches = useMediaQuery('(min-width:600px)');
+  const isMobile = useMediaQuery('(min-width:756px)');
   const [movies, setMovies] = useState([]);
   const [open, setOpen] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState(null);
@@ -29,40 +26,45 @@ export function UpcomingMovieList() {
     getUpcomingMovies().then((response) => {
       setMovies(response.data);
     }).catch((error) => {
-     alert('서버 문제로 영화 정보를 가져오지 못했습니다. 다시 시도해주세요.');
+      alert('서버 문제로 영화 정보를 가져오지 못했습니다. 다시 시도해주세요.');
     });
   }, []);
 
 
   return (
-      <Container>
-        <ImageList sx={{ width: '100%', height: '100%' }} cols={matches ? 3 : 2}
-          rowHeight={matches ? 600 : 300}>
-          <ImageListItem key="Subheader" cols={3} style={{ height: 'auto' }}>
-            <ListSubheader component="div">개봉 예정 영화</ListSubheader>
+    <Container>
+      <ListSubheader sx={{ fontSize: '1.5rem' }}>개봉 예정 영화</ListSubheader>
+      <ImageList className="image-list" sx={{
+        width: '100%',
+        height: '100%',
+        display: 'flex', // flex 레이아웃으로 변경
+        flexDirection: isMobile ? 'row' : 'column', // 방향 설정
+        flexWrap: 'nowrap', // 한 줄로 배치
+        overflowY: isMobile ? 'auto' : 'initial', // 세로 스크롤 설정
+        gap: 10,
+      }}>
+        {movies.length > 0 && movies.map((movie) => (
+          <ImageListItem className='image-item' key={movie.id} onClick={() => handleClickOpen(movie)} sx={{ height: 150 }}>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.posterPath}`} alt={movie.title} />
+            <ImageListItemBar
+              title={movie.title}
+              subtitle={movie.releaseDate}
+              position="overlay"
+            />
           </ImageListItem>
-          {movies.length > 0 && movies.map((movie) => (
-            <ImageListItem key={movie.id} onClick={() => handleClickOpen(movie)}>
-              <img src={`https://image.tmdb.org/t/p/w500${movie.posterPath}`} alt={movie.title} />
-              <ImageListItemBar
-                title={movie.title}
-                subtitle={movie.releaseDate}
-                position="overlay"
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>{selectedMovie?.title}</DialogTitle>
-          <DialogContent>
-            {selectedMovie?.overview}
-          </DialogContent>
-        </Dialog>
-      </Container>
+        ))}
+      </ImageList>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{selectedMovie?.title}</DialogTitle>
+        <DialogContent>
+          {selectedMovie?.overview}
+        </DialogContent>
+      </Dialog>
+    </Container>
   );
+
 }
-
-
 async function getUpcomingMovies() {
-  return await axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/api/upcoming`);
+  // return await axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/api/upcoming`);
+  return await axios.get('http://localhost:8081/api/upcoming')
 }
