978d9baf5a0c903c8a36e73e5d22ef3d386b407c
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-05-25 12:26:17 +0900

    fix: mui styled 모듈 수정

--- horror-alram-front/src/components/StreamingTimeline.jsx
+++ horror-alram-front/src/components/StreamingTimeline.jsx
@@ -6,10 +6,10 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import axios from 'axios';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
-import {Link} from 'react-router-dom';
-import { styled } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
 
 const Div = styled('div')({
   color: 'white',
@@ -20,32 +20,32 @@ export function StreamingTimeline() {
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/api/streaming/expired`)
-    .then((response) => {
-      setMovies(response.data.expiredMovies);
-    }).catch((error) => {
-      alert('서버 문제로 영화 정보를 가져오지 못했습니다. 다시 시도해주세요.');
-    });
+      .then((response) => {
+        setMovies(response.data.expiredMovies);
+      }).catch((error) => {
+        alert('서버 문제로 영화 정보를 가져오지 못했습니다. 다시 시도해주세요.');
+      });
   }, []);
 
   return (
-    movies.length === 0 ? 
+    movies.length === 0 ?
       <Div >종료 예정인 스트리밍이 없습니다</Div> :
       <Timeline position="alternate">
         {movies.map((movie) => (
-            <TimelineItem key={movie.id}>
-              <TimelineSeparator>
-                <TimelineDot color="primary"/>
-                <TimelineConnector/>
-              </TimelineSeparator>
-              <TimelineContent>
-                <Link to={`/movie/${movie.id}`}
-                      style={{textDecoration: 'none', color: 'black'}}
-                >
-                  <Typography>{movie.title}</Typography>
-                </Link>
-                <Typography>{movie.expiredDate}</Typography>
-              </TimelineContent>
-            </TimelineItem>
+          <TimelineItem key={movie.id}>
+            <TimelineSeparator>
+              <TimelineDot color="primary" />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Link to={`/movie/${movie.id}`}
+                style={{ textDecoration: 'none', color: 'black' }}
+              >
+                <Typography>{movie.title}</Typography>
+              </Link>
+              <Typography>{movie.expiredDate}</Typography>
+            </TimelineContent>
+          </TimelineItem>
         ))}
       </Timeline>
   );
