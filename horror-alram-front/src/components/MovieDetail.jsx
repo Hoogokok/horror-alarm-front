d08714526f346537653f9e4b7657419bd02610d0
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-03 15:53:51 +0900

    style: 영화 상세 정보 페이지의 배경화면색과 글자색을 기존 스타일과 일관성 있게 바꾸었다

--- horror-alram-front/src/components/MovieDetail.jsx
+++ horror-alram-front/src/components/MovieDetail.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import {useEffect, useState,} from "react";
-import {useParams} from 'react-router-dom';
+import { useEffect, useState, } from "react";
+import { useParams } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import {styled} from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import { styled } from '@mui/material/styles';
 
 const Img = styled('img')({
   margin: 'auto',
@@ -15,51 +16,46 @@ const Img = styled('img')({
 
 export default function Detail() {
   const [movie, setMovie] = useState({});
-  const {id} = useParams();
+  const { id } = useParams();
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/api/streaming/expired/detail/${id}`)
-    .then((response) => {
-      setMovie(response.data);
-    }).catch((error) => {
-      alert('서버 문제로 영화 정보를 가져오지 못했습니다. 다시 시도해주세요.');
-    });
+      .then((response) => {
+        setMovie(response.data);
+      }).catch((error) => {
+        alert('서버 문제로 영화 정보를 가져오지 못했습니다. 다시 시도해주세요.');
+      });
   }, [id]);
 
   return (
-      <Paper sx={
-        {
-          p:2,
-          margin: 'auto',
-          marginTop: 10,
-          maxWidth: 500,
-          flexGrow: 1,
-          backgroundColor (theme) {
-            return theme.palette.mode === 'light'
-                ? theme.palette.grey[50]
-                : theme.palette.grey[800];
-          }
-        }
-      }>
-        <Grid container spacing={2}>
+    <Paper
+      sx={{
+        p: 2,
+        margin: 'auto',
+        marginTop: 5,
+        maxWidth: 500,
+        flexGrow: 3,
+        backgroundColor: '#1D3131'
+      }}
+    >
+      <Grid container spacing={3}>
+        <Grid item xs={7}>
+          <Img src={`${process.env.REACT_APP_POSTER_API_URL}${movie.posterPath}`} alt={movie.title} />
+        </Grid>
+        <Grid item xs container direction="row" spacing={2}>
+          <Grid item xs>
+            <Typography gutterBottom variant="h5" color={'white'}>
+              {movie.title}
+            </Typography>
+          </Grid>
           <Grid item>
-          <img src={`${process.env.REACT_APP_POSTER_API_URL}${movie.posterPath}`} alt={movie.title} />          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="h5">
-                  {movie.title}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body2" style={{cursor: 'pointer'}}>
-                  {movie.overview}
-                </Typography>
-              </Grid>
-            </Grid>
+            <Typography variant="body2" style={{ cursor: 'pointer' }} color={'white'}>
+              {movie.overview}
+            </Typography>
           </Grid>
         </Grid>
-      </Paper>
+      </Grid>
+    </Paper>
   );
 }
 
