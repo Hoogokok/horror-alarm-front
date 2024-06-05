f9c12b4aad8735ed094ad33afc7b81919a72c953
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-05 18:29:04 +0900

    refactor: 개봉 예정 영화 컴포넌트를 복사한 후 props 주입 방식으로 영화 이미지 리스트와 영화 설명 다이얼로그를 렌더링하게 바꿈

---
+++ horror-alram-front/src/components/ImageList.jsx
@@ -0,0 +1,36 @@
+import { ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
+import { useMediaQuery } from '@mui/material';
+
+export default function UpcomingImageList({ movies, error, handleOpen }) {
+    const isMobile = useMediaQuery('(min-width:756px)');
+    console.log(movies);
+    console.log(error);
+    const handleClickOpen = (movie) => {
+        handleOpen(movie);
+    }
+
+    return (
+        <ImageList className="image-list" sx={{
+            width: '100%',
+            height: '100%',
+            display: 'flex', // flex 레이아웃으로 변경
+            flexDirection: isMobile ? 'row' : 'column', // 방향 설정
+            flexWrap: 'nowrap', // 한 줄로 배치
+            overflowY: isMobile ? 'auto' : 'initial', // 세로 스크롤 설정
+            gap: 10,
+        }}>
+            {error && <div>서버 문제로 영화 정보를 가져오지 못했습니다. 다시 시도해주세요.</div>}
+            {!error && movies.length === 0 && <div>개봉 예정 영화가 없습니다.</div>}
+            {!error && movies.length > 0 && movies.map((movie) => (
+                <ImageListItem className='image-item' key={movie.id} onClick={() => handleClickOpen(movie)} sx={{ height: 150 }}>
+                    <img src={`${process.env.REACT_APP_POSTER_API_URL}${movie.posterPath}`} alt={movie.title} />
+                    <ImageListItemBar
+                        title={movie.title}
+                        subtitle={movie.releaseDate}
+                        position="overlay"
+                    />
+                </ImageListItem>
+            ))}
+        </ImageList>
+    );
+}
---
+++ horror-alram-front/src/components/UpcomingMovieList copy.jsx
@@ -0,0 +1,20 @@
+import Container from '@mui/material/Container';
+import { ListSubheader } from '@mui/material';
+
+export function UpcomingMovieList({ imageList, movieOverViewDialog }) {
+
+  return (
+    <Container>
+      <ListSubheader sx={
+        {
+          backgroundColor: '#2F4F4F',
+          textAlign: 'center',
+          color: 'white',
+        }
+      }> 개봉 예정 영화</ ListSubheader>
+      {imageList}
+      {movieOverViewDialog}
+    </Container>
+  );
+
+}
