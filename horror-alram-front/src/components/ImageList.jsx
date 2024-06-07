082c36909b98b597c17920ad22da36ef02af949f
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-07 16:21:14 +0900

    style: 커스텀 컴포넌트 Div로 기본 마진값과 텍스트 컬러를 수정

--- horror-alram-front/src/components/ImageList.jsx
+++ horror-alram-front/src/components/ImageList.jsx
@@ -1,11 +1,18 @@
 import { ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
 import { useMediaQuery } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 export default function UpcomingImageList({ movies, error, handleOpen }) {
     const isMobile = useMediaQuery('(min-width:756px)');
     const handleClickOpen = (movie) => {
         handleOpen(movie);
     }
+
+    const Div = styled('div')({
+        margin: 10,
+        color: 'white',
+        textAlign: 'center',
+    });
 
     return (
         <ImageList className="image-list" sx={{
@@ -17,8 +24,8 @@ export default function UpcomingImageList({ movies, error, handleOpen }) {
             overflowY: isMobile ? 'auto' : 'initial', // 세로 스크롤 설정
             gap: 10,
         }}>
-            {error && <div>서버 문제로 영화 정보를 가져오지 못했습니다. 다시 시도해주세요.</div>}
-            {!error && movies.length === 0 && <div>개봉 예정 영화가 없습니다.</div>}
+            {error && <Div>서버 문제로 영화 정보를 가져오지 못했습니다. 다시 시도해주세요.</Div>}
+            {!error && movies.length === 0 && <Div>개봉 예정 영화가 없습니다.</Div>}
             {!error && movies.length > 0 && movies.map((movie) => (
                 <ImageListItem className='image-item' key={movie.id} onClick={() => handleClickOpen(movie)} sx={{ height: 150 }}>
                     <img src={`${process.env.REACT_APP_POSTER_API_URL}${movie.posterPath}`} alt={movie.title} />
