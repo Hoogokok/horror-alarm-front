246286d7fc28aac82cdd8fac9226c70c81058696
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-07 16:13:19 +0900

    refactor: App js에서 스트리밍 종료 예정 데이터를 받아 MainTabs 컴포넌트에 주입

--- horror-alram-front/src/App.js
+++ horror-alram-front/src/App.js
@@ -3,6 +3,7 @@ import MainTabs from "./components/MainTab.jsx";
 import { createGlobalStyle } from 'styled-components';
 import Footer from './components/Footer.jsx';
 import getUpcomingMovies from './functions/upcoming.js';
+import getExpiringMovies from './functions/expiring.js';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -11,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const upcomingMovies = await getUpcomingMovies();
+const expiringMovies = await getExpiringMovies();
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
       <GlobalStyle />
       <MainTabs 
         upcomingMovies={upcomingMovies}
+        streamingMovies={expiringMovies}
       />
       <Footer />
     </BrowserRouter>
