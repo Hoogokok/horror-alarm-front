e9aea20e35a20b35b0410b8b7873e1dbd45fb8d8
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-12 18:21:07 +0900

    refactor: 리액트 쿼리 의존성과 쿼리에 넣을 함수를 가져오고 초기화에 필요한 리액트 쿼리를 작성함

--- horror-alram-front/src/components/AlramSwitchs.jsx
+++ horror-alram-front/src/components/AlramSwitchs.jsx
@@ -2,6 +2,14 @@ import Container from '@mui/material/Container';
 import FormGroup from '@mui/material/FormGroup';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useMediaQuery } from '@mui/material';
+import {
+  handleAlarmPermission,
+  handleUpcomingMovieSubscribe,
+  handleNetflixSubscribe,
+  handleInitialSubscription
+} from "../functions/messaging";
+import AlramSwitch from "./AlramSwitch"
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
 const theme = createTheme({
   palette: {
@@ -11,8 +19,14 @@ const theme = createTheme({
   },
 });
 
-export function AlramSwitchs({ alarmPermissionSwitch, upcomingSubscriptionSwitch, netflixSubscriptionSwitch }) {
+export function AlramSwitchs({ intialSubscription, alarmPermissionSwitch, upcomingSubscriptionSwitch, netflixSubscriptionSwitch }) {
   const isMobile = useMediaQuery('(min-width:756px)');
+  const queryClient = useQueryClient()
+  const { data: intialSubscription, isLoading: initialLoading, isError: initialError } = useQuery({
+    queryKey: 'initialSubscription',
+    queryFn: handleInitialSubscription,
+  });
+
 
   return (<Container>
     <FormGroup sx={
