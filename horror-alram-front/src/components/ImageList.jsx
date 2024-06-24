513301ac085e7c15075a8c6abe376a8196facc8e
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-24 18:00:33 +0900

    style: 이미지 리스트 컴포넌트 하단에 여백을 줘서 모바일 환경에서 마지막 상영관 정보가 보이지 않던 부분을 수정

--- horror-alram-front/src/components/ImageList.jsx
+++ horror-alram-front/src/components/ImageList.jsx
@@ -16,20 +16,25 @@ export default function MovieImageList({ movies, error, handleOpen, guideText })
 
     return (
         <ImageList className="image-list" sx={{
-            width: '100%',
-            height: '100%',
-            display: 'flex', // flex 레이아웃으로 변경
+            display: 'flex',
             flexDirection: isMobile ? 'row' : 'column', // 방향 설정
             flexWrap: 'nowrap', // 한 줄로 배치
             overflowY: isMobile ? 'auto' : 'initial', // 세로 스크롤 설정
             gap: 10,
+            '@media (max-width:600px)': {
+                paddingBottom: '64px', // 하단 여백 값, 필요에 따라 조정
+            }
         }}>
             {error && <Div>서버 문제로 영화 정보를 가져오지 못했습니다. 다시 시도해주세요.</Div>}
             {!error && movies.length === 0 && <Div>{guideText}</Div>}
             {!error && movies.length > 0 && movies.map((movie) => (
                 <>
                     <ImageListItem className='image-item' key={movie.id} sx={{ height: 150 }}>
-                        <img src={`${process.env.REACT_APP_POSTER_API_URL}${movie.posterPath}`} alt={movie.title} onClick={() => handleClickOpen(movie)} loading="lazy" />
+                        <img src={`${process.env.REACT_APP_POSTER_API_URL}${movie.posterPath}`}
+                            alt={movie.title} onClick={() => handleClickOpen(movie)}
+                            loading="lazy"
+
+                        />
                         <ImageListItemBar
                             title={movie.title}
                             subtitle={movie.releaseDate}
@@ -37,7 +42,7 @@ export default function MovieImageList({ movies, error, handleOpen, guideText })
                             onClick={() => handleClickOpen(movie)}
                         />
                         <ImageListItemBar
-                            sx={{color: 'white'}}
+                            sx={{ color: 'white' }}
                             title={movie.theaters.length > 0 ? '상영관' : '상영관 정보 없음'}
                             subtitle={movie.theaters.length > 0 ? movie.theaters.join(', ') : '상영관 정보 없음'}
                             position="below"
