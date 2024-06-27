5f43640ea957e882b71ab525c7e2815a3966b3b9
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-27 19:38:23 +0900

    style: 하단 여백을 기본으로 주고, 포스터 이미지 높이를 모바일에는 auto로 수정

--- horror-alram-front/src/components/ImageList.jsx
+++ horror-alram-front/src/components/ImageList.jsx
@@ -21,15 +21,16 @@ export default function MovieImageList({ movies, error, handleOpen, guideText })
             flexWrap: 'nowrap', // 한 줄로 배치
             overflowY: isMobile ? 'auto' : 'initial', // 세로 스크롤 설정
             gap: 10,
-            '@media (max-width:600px)': {
-                paddingBottom: '64px', // 하단 여백 값, 필요에 따라 조정
-            }
+            paddingBottom: '64px', // 하단 여백 값, 필요에 따라 조정
         }}>
             {error && <Div>서버 문제로 영화 정보를 가져오지 못했습니다. 다시 시도해주세요.</Div>}
             {!error && movies.length === 0 && <Div>{guideText}</Div>}
             {!error && movies.length > 0 && movies.map((movie) => (
                 <>
-                    <ImageListItem className='image-item' key={movie.id} sx={{ height: 150 }}>
+                    <ImageListItem className='image-item' key={movie.id} sx={{
+                        height: "100%", // 이미지 높이 설정
+                        width: isMobile ? 'auto' : '100%', // 이미지 너비 설정
+                    }}>
                         <img src={`${process.env.REACT_APP_POSTER_API_URL}${movie.posterPath}`}
                             alt={movie.title} onClick={() => handleClickOpen(movie)}
                             loading="lazy"
