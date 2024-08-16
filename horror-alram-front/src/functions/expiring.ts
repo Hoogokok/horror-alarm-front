e1f7000c799104a536c76ddd898ff5772a326bc0
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-16 10:15:08 +0900

    refactor: 스트리밍 종료 예정 영화 타입과 응답 예외 타입을 추가하고 응답 예외 타입을 반환하는 로직을 추가했다.

--- horror-alram-front/src/functions/expiring.ts
+++ horror-alram-front/src/functions/expiring.ts
@@ -1,11 +1,25 @@
 import axios from 'axios';
+import { ResponseError } from './upcoming';
 
-interface  ExpiringMovieResponse {
-    movies: Array<any>;
-    error: any;
+/*
+id: 영화 아이디(long)
+title: 영화 제목(문자열)
+posterPath: 영화 포스터 uri
+expiredDate: 스트리밍 종료일
+*/
+
+interface ExpiringMovie {
+    id: number;
+    title: string;
+    posterPath: string;
+    expiredDate: string;
+}
+interface ExpiringMovieResponse {
+    movies: Array<ExpiringMovie>;
+    error: ResponseError | undefined;
 }
 
-export default async function getExpiringMovies() {
+export default async function getExpiringMovies(): Promise<ExpiringMovieResponse> {
     const expiringMovies: ExpiringMovieResponse = {
         movies: [],
         error: undefined
@@ -15,8 +29,21 @@ export default async function getExpiringMovies() {
         expiringMovies.movies = response.data;
         return expiringMovies;
     } catch (error) {
-        console.error('만료된 영화 에러 발생');
-        expiringMovies.error = error;
+        if (axios.isAxiosError(error)) {
+            expiringMovies.error = {
+                data: {
+                    message: error.message,
+                    status: error.response?.status || 500
+                }
+            };
+            return expiringMovies;
+        }
+        expiringMovies.error = {
+            data: {
+                message: '알 수 없는 에러',
+                status: 500
+            }
+        };
         return expiringMovies;
     }
 }
