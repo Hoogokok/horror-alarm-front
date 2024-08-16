75c5b404798ff58ada27f107d1d91bcf7ddc0469
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-16 13:24:39 +0900

    refactor: 파라미터 타입을 정의하고 반환 타입을 명시했다.

--- horror-alram-front/src/components/MovieList.tsx
+++ horror-alram-front/src/components/MovieList.tsx
@@ -1,8 +1,13 @@
 import Container from '@mui/material/Container';
 import { ListSubheader } from '@mui/material';
+interface MovieListProps {
+  imageList: JSX.Element;
+  movieOverViewDialog: JSX.Element;
+  header: any;
+}
 
 
-export function MovieList({ imageList, movieOverViewDialog, header }) {
+export function MovieList({ imageList, movieOverViewDialog, header }: MovieListProps) : JSX.Element {
 
   return (
     <Container>
