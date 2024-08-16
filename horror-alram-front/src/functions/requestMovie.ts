302bae3fde755b8f8a40426d03d72c01d01a5a56
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-16 14:14:30 +0900

    feat: 영화 정보를 요청하는 공통된 로직을 하나의 함수로 작성했다.

---
+++ horror-alram-front/src/functions/requestMovie.ts
@@ -0,0 +1,52 @@
+import axios, { AxiosResponse } from 'axios';
+import { Movies, ResponseError } from '../type/movie';
+
+export default async function requestMovieInfo(request: () => Promise<AxiosResponse | ResponseError>): Promise<Movies> {
+    const requestResultInfo: Movies = {
+        movies: [],
+        error: {
+            data: {
+                message: '',
+                status: 0
+            },
+            isError: false
+        }
+    };
+    const response: AxiosResponse | ResponseError = await requestMovieApi(request);
+    if ((response as ResponseError).isError !== undefined) {
+        requestResultInfo.error = response as ResponseError;
+        return requestResultInfo;
+    }
+    requestResultInfo.movies = response.data;
+    return requestResultInfo;
+}
+
+export async function requestMovieApi(
+    request: () => Promise<AxiosResponse | ResponseError>
+): Promise<AxiosResponse | ResponseError> {
+    try {
+        return request();
+    }
+    catch (error) {
+        return handleResponseError(error);
+    }
+}
+
+function handleResponseError(error: unknown): ResponseError {
+    if (axios.isAxiosError(error)) {
+        return {
+            data: {
+                message: error.message,
+                status: error.response?.status || 500
+            },
+            isError: true
+        };
+    }
+    return {
+        data: {
+            message: '알 수 없는 에러',
+            status: 500
+        },
+        isError: true
+    };
+}
