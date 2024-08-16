dbf6960b6c9c5964e199a00cda4c3f28d2d6e883
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-16 13:58:39 +0900

    refactor: 파라미터 타입과 반환 타입을 작성했다.

--- horror-alram-front/src/components/MovieOverViewDialog.tsx
+++ horror-alram-front/src/components/MovieOverViewDialog.tsx
@@ -1,14 +1,21 @@
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
+import { MovieResponse } from '../functions/upcoming';
 
-export default function MovieOverViewDialog({ open, selectedMovie, handleClose }) {
-      return (
-     <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>{selectedMovie?.title}</DialogTitle>
-        <DialogContent>
-          {selectedMovie?.overview}
-        </DialogContent>
-     </Dialog>
-      );
-    }
+interface MovieOverViewDialogProps {
+  open: boolean;
+  selectedMovie: MovieResponse;
+  handleClose: () => void;
+}
+
+export default function MovieOverViewDialog({ open, selectedMovie, handleClose }: MovieOverViewDialogProps): JSX.Element {
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>{selectedMovie?.title}</DialogTitle>
+      <DialogContent>
+        {selectedMovie?.overview}
+      </DialogContent>
+    </Dialog>
+  );
+}
