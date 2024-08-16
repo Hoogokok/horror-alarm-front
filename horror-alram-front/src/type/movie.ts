64009e04a3e0ec98c49c8d806f34d3af5db62c3c
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-16 14:08:39 +0900

    refactor: 영화와 관련된 타입을 하나의 파일로 구성했다.

---
+++ horror-alram-front/src/type/movie.ts
@@ -0,0 +1,32 @@
+export interface MovieResponse {
+    id: number;
+    title: string;
+    releaseDate: string;
+    posterPath: string;
+    overview: string;
+    theaters: Array<string>;
+}
+
+export type ResponseError = {
+    data?: {
+        message: string;
+        status: number;
+    };
+    isError: boolean;
+}
+
+export interface Movies {
+    movies: MovieResponse[];
+    error: ResponseError;
+}
+
+export interface ExpiringMovie {
+    id: number;
+    title: string;
+    posterPath: string;
+    expiredDate: string;
+}
+export interface ExpiringMovieResponse {
+    movies: Array<ExpiringMovie>;
+    error: ResponseError;
+}
