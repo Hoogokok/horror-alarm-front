108e55dc8616d031f1ff2cfeeee9503f20d394d5
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-19 18:25:23 +0900

    refactor: 타입을 추가하고 타입에 맞게 로직을 수정했다.

--- horror-alram-front/src/components/MainTab.tsx
+++ horror-alram-front/src/components/MainTab.tsx
@@ -1,26 +1,27 @@
-import {
-  Route,
-  Link,
-  useLocation,
-  Routes,
-} from 'react-router-dom'
-import axios from 'axios';
-import Container from '@mui/material/Container';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import { AlramSwitchs } from "./AlarmSwitchs"
-import { MovieList } from "./MovieList"
-import MovieImageList from './ImageList';
-import MovieOverViewDialog from './MovieOverViewDialog';
-import { StreamingTimeline } from "./StreamingTimeline"
-import Detail from "./MovieDetail"
-import { useEffect, useState } from "react";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import getExpiringMovies from '../functions/expiring';
-import requestMovieApi from '../functions/requestMovie';
-import { useQuery } from '@tanstack/react-query';
 import { useMediaQuery } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
+import Container from '@mui/material/Container';
+import { createTheme } from '@mui/material/styles';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { SetStateAction, useEffect, useState } from "react";
+import {
+  Link,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router-dom';
+import getExpiringMovies from '../functions/expiring';
+import requestMovieApi from '../functions/requestMovie';
+import { MovieResponse } from '../type/movie';
+import { AlramSwitchs } from "./AlarmSwitchs";
+import MovieImageList from './ImageList';
+import Detail from "./MovieDetail";
+import { MovieList } from "./MovieList";
+import MovieOverViewDialog from './MovieOverViewDialog';
+import { StreamingTimeline } from "./StreamingTimeline";
 
 
 const theme = createTheme({
@@ -47,15 +48,36 @@ const theme = createTheme({
 
 
 export default function MainTabs() {
-  const { data: upcomingMovies, isLoading: upcomingMoviesLoading, error: upcomingMoviesError } = useQuery({
+  const { data: upcomingMovies = {
+    movies: [],
+    error: { isError: false },
+  }, isLoading: upcomingMoviesLoading, error: upcomingMoviesError } = useQuery({
     queryKey: ['upcomingMovies'],
     queryFn: async () => requestMovieApi(async () => await axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/api/upcoming`)),
   });
-  const { data: streamingMovies, isLoading: streamingMoviesLoading, error: streamingMoviesError } = useQuery({
+
+  const { data: streamingMovies = {
+    movies: {
+      expiredMovies: []
+    }, error: { isError: false },
+  }, isLoading: streamingMoviesLoading, error: streamingMoviesError } = useQuery({
     queryKey: ['streamingMovies'],
-    queryFn: async () =>  getExpiringMovies(),
+    queryFn: async () => getExpiringMovies(),
   });
-  const { data: releasingMovies, isLoading: releasingMoviesLoading, error: releasingMoviesError } = useQuery({
+
+  // 데이터 변환 함수
+  const transformStreamingMovies = (data: any) => {
+    if (Array.isArray(data)) {
+      return { expiredMovies: { expiredMovies: data } };
+    }
+    return data;
+  };
+
+
+  const { data: releasingMovies = {
+    movies: [],
+    error: { isError: false },
+  }, isLoading: releasingMoviesLoading, error: releasingMoviesError } = useQuery({
     queryKey: ['releasingMovies'],
     queryFn: async () => requestMovieApi(async () => await axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/api/releasing`)),
   });
@@ -67,7 +89,14 @@ export default function MainTabs() {
   const [value, setValue] = useState(getTabValue(path));
   // 영화 상세 정보 다이얼로그에 쓰이는 state
   const [open, setOpen] = useState(false);
-  const [selectedMovie, setSelectedMovie] = useState({});
+  const [selectedMovie, setSelectedMovie] = useState({
+    id: 0,
+    title: '',
+    overview: '',
+    posterPath: '',
+    theaters: [''],
+    releaseDate: '',
+  });
 
 
   useEffect(() => {
@@ -82,10 +111,10 @@ export default function MainTabs() {
     return <div>서버 문제로 정보를 가져올 수 없습니다.</div>;
   }
 
-  const handleChange = (event, newValue) => {
+  const handleChange = (event: any, newValue: SetStateAction<string>) => {
     setValue(newValue);
   };
-  const handleOpen = (movie) => {
+  const handleOpen = (movie: MovieResponse) => {
     setSelectedMovie(movie);
     setOpen(true);
   };
@@ -96,39 +125,37 @@ export default function MainTabs() {
 
   return (
     <Container>
-      <ThemeProvider theme={theme}>
-        <Tabs
-          value={value} onChange={handleChange}
-          indicatorColor="secondary"
-          textColor="white"
-          variant={isDesktop ? "fullWidth" : "scrollable"}
-          allowScrollButtonsMobile
-          scrollButtons
-          centered
-        >
-          <Tab label="개봉 예정" value="upcoming" component={Link} to="/upcoming" />
-          <Tab label="개봉중" value="releasing" component={Link} to="/releasing" />
-          <Tab label="알람 설정" value="alram" component={Link} to="/alram" />
-          <Tab label="스트리밍 종료 예정" value="streamingexpired" component={Link}
-            to="/streamingexpired" />
-        </Tabs>
-      </ThemeProvider>
+      <Tabs
+        value={value} onChange={handleChange}
+        indicatorColor="secondary"
+        textColor="inherit"
+        variant={isDesktop ? "fullWidth" : "scrollable"}
+        allowScrollButtonsMobile
+        scrollButtons
+        centered
+      >
+        <Tab label="개봉 예정" value="upcoming" component={Link} to="/upcoming" />
+        <Tab label="개봉중" value="releasing" component={Link} to="/releasing" />
+        <Tab label="알람 설정" value="alram" component={Link} to="/alram" />
+        <Tab label="스트리밍 종료 예정" value="streamingexpired" component={Link}
+          to="/streamingexpired" />
+      </Tabs>
       <Routes>
         <Route path="upcoming" element={<MovieList
           imageList={<MovieImageList movies={upcomingMovies.movies} error={upcomingMovies.error}
             handleOpen={handleOpen} guideText={"개봉 예정 영화가 없습니다."} />}
           movieOverViewDialog={<MovieOverViewDialog open={open} handleClose={handleClose}
-            selectedMovie={selectedMovie} />}
-        />} />
+            selectedMovie={selectedMovie} />} header={undefined} />} />
         <Route path="releasing" element={<MovieList
           imageList={<MovieImageList movies={releasingMovies.movies} error={releasingMovies.error}
             handleOpen={handleOpen} guideText={"개봉 중인 영화가 없습니다."} />}
           movieOverViewDialog={<MovieOverViewDialog open={open} handleClose={handleClose}
-            selectedMovie={selectedMovie} />}
-        />} />
+            selectedMovie={selectedMovie} />} header={undefined} />} />
         <Route path="alram" element={<AlramSwitchs />} />
         <Route path="streamingexpired" element={<StreamingTimeline
-          movies={streamingMovies.movies} error={streamingMovies.error}
+          movies={
+            transformStreamingMovies(streamingMovies).movies
+          } error={streamingMovies.error}
         />} />
         <Route path="movie/:id" element={<Detail />} />
       </Routes>
@@ -136,7 +163,7 @@ export default function MainTabs() {
   );
 }
 
-function getTabValue(path) {
+function getTabValue(path: string) {
   if (path === 'upcoming') {
     return 'upcoming';
   } else if (path === 'alram') {
@@ -149,7 +176,7 @@ function getTabValue(path) {
   return '/';
 }
 
-function getTabPath(value) {
+function getTabPath(value: string) {
   if (value === 'upcoming') {
     return 'upcoming';
   } else if (value === 'alram') {
